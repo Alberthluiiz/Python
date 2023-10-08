e133060a91a221905c6b2b3ebe453ebe63b987f9
Albertluiiz <luisguillen290301@gmail.com>
2023-10-08 12:36:06 -0500

    Practicas

---
+++ 00_hello.py
@@ -0,0 +1,16 @@
+# Esto es un comentario: Hola Mundo
+
+print("Hola Mundo");
+print("Hola Mundo");
+
+
+""" Este es un comentario en varias lineas """
+'''Este tambien es un comentario en varias lineas'''
+
+# Checking data types
+# Consulta el tipo de dato
+print(type("Sou un dato str")); #Tipo Str
+print(type(5)); #Tipo int
+print(type(1.5)); #Tipo float
+print(type(True)); #Tipo Bool
+print(type([1, 2, 3, 4, 5])); #Tipo List
